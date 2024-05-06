bcef26a78dfb0be1002ef068c88710b5635185d7
Ziyeon <ziyeon.ivi@gmail.com>
2024-05-06 20:45:56 +0900

    🚧 stop event bubbling

--- js/bottom-menu.js
+++ js/bottom-menu.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-  $(".bottom-menu__item").click(function () {
+  $(".bottom-menu__item").click(function (event) {
+    event.stopPropagation();
+
     $(this).find(".nest-menu__list").toggleClass("active");
     $(this).find(".btn--bottom").toggleClass("active");
 
@@ -10,5 +12,9 @@ $(document).ready(function () {
       .end()
       .find(".btn--bottom")
       .removeClass("active");
+  });
+
+  $(".nest-menu__list").click(function (event) {
+    event.stopPropagation();
   });
 });
